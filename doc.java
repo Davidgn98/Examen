8846dc8a8c3c1ef62f9ee336baf3343924a4750a
David <davidiescomercio@gmail.com>
2020-03-10 16:40:16 +0100

    DocDocumentado

--- doc.java
+++ doc.java
@@ -2,13 +2,18 @@
 /**
  * Write a description of class doc here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author (David) 
+ * @version (10/03/2020 1.0v)
  */
 public interface doc
 {
    
    // Devuelve el contenido de la fila x del fichero de texto con ruta absoluta file.
+   /**
+    * @param x el numero de la fila del fichero
+    * @param file la ruta absoluta del fichero
+    * @return devuelve la linea x del fichero en la ruta file
+    */
    public abstract String obtenerLinea(int x, String file);
    
    
